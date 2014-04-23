c68bd29379d7acb77cc9cfb62ca73a9d37192a81
Matias Lespiau <mlespiau@avature.net>
2014-04-23 14:33:58 -0300

    Remove unused function

--- get_label_from_code.R
+++ get_label_from_code.R
@@ -2,8 +2,3 @@ meaningfulLabels <- read.table("./UCI HAR Dataset/activity_labels.txt", sep=" ")
 GetLabelFromCode <- function(code) {
   as.character(meaningfulLabels[meaningfulLabels$V1 == code[1],2])
 }
-ApplyLabelsToCodeVector <- function(y) {
-  for (i in y) {
-    y[i] <- GetLabelFromCode(y[i])  
-  }
-}
